8b9306c2ea9762860f46b4638944a3e96e7f1809
Soniyasunny1 <90353196+Soniyasunny1@users.noreply.github.com>
2022-01-09 18:56:30 -0700

    new .py file

---
+++ utils.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import numpy as np
+import math
+import sklearn.datasets
+import ipywidgets as widgets
+
+##Seaborn for fancy plots. 
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.rcParams["figure.figsize"] = (8,8)
+
+def sklearn_to_df(sklearn_dataset):
+    df = pd.DataFrame(sklearn_dataset.data, columns=sklearn_dataset.feature_names)
+    df['target'] = pd.Series(sklearn_dataset.target)
+    return df
+
+class myeda:
+ 
+    def __init__(self, data, target):
+        self.data = data
+        self.target = target
+        self.cat = []
+        self.num = []
+
+    def info(self):
+        return self.data.info()
+
+    def giveTarget(self):
+        return self.target
+        
+    def setCat(self, catList):
+        self.cat = catList
+    
+    def setNum(self, numList):
+        self.num = numList
+
+    def countPlots(self, splitTarg=False, show=True):
+        n = len(self.cat)
+        cols = 2
+        figure, ax = plt.subplots(math.ceil(n/cols), cols)
+        r = 0
+        c = 0
+        for col in self.cat:
+            if splitTarg == False:
+                sns.countplot(data=self.data, x=col, ax=ax[r][c])
+            if splitTarg == True:
+                sns.countplot(data=self.data, x=col, hue=self.target, ax=ax[r][c])
+            c += 1
+            if c == cols:
+                r += 1
+                c = 0
+        if show == True:
+            figure.show()
+        return figure
+
+    def histPlots(self, kde=True, splitTarg=False, show=True):
+        n = len(self.num)
+        cols = 2
+        figure, ax = plt.subplots(math.ceil(n/cols), cols)
+        r = 0
+        c = 0
+        for col in self.num:
+            #print("r:",r,"c:",c)
+            if splitTarg == False:
+                sns.histplot(data=self.data, x=col, kde=kde, ax=ax[r][c])
+            if splitTarg == True:
+                sns.histplot(data=self.data, x=col, hue=self.target, kde=kde, ax=ax[r][c])
+            c += 1
+            if c == cols:
+                r += 1
+                c = 0
+        if show == True:
+            figure.show()
+        return figure
+
+    def fullEDA(self):
+        out1 = widgets.Output()
+        out2 = widgets.Output()
+        out3 = widgets.Output()
+        out4 = widgets.Output()
+
+        tab = widgets.Tab(children = [out1, out2, out3])
+        tab.set_title(0, 'Info')
+        tab.set_title(1, 'Categorical')
+        tab.set_title(2, 'Numerical')
+        display(tab)
+
+        with out1:
+            self.info()
+
+        with out2:
+            fig2 = self.countPlots(splitTarg=True, show=False)
+            plt.show(fig2)
+        
+        with out3:
+            fig3 = self.histPlots(kde=True, show=False)
+            plt.show(fig3)
